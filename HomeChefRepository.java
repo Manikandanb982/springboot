02e06deaa59e24d207fcbd79503aa43cbd3a3150
Manikandan B <717822d127@kce.ac.in>
2025-07-02 14:38:46 +0530

    Add files via upload

---
+++ HomeChefRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.HomeChef;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomeChefRepository extends JpaRepository<HomeChef, Long> {
+   
+List<HomeChef> findByLocation(String location);
+    List<HomeChef> findByNameContainingIgnoreCase(String name);
+    boolean existsByContactNumber(String contactNumber);
+
+
+}
